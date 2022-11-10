f9ed38191fa62a590e97b51aebd86da2340626d5
nihal-flycatchtech <nihal.m@flycatchtech.com>
2022-11-10 08:23:27 +0530

    fix:art:chatbot implementation

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const ChatBotConstants = {
+    BOT: 'bot',
+    RECEIVER: 'reciever',
+    EMAIL_REQUEST: 'Please enter contact details ',
+    TITLE_REQUEST: 'What is this issue regarding?',
+    DESCRIPTION_REQUEST: 'Please describe the issue',
+    TICKET_NUMBER_RESPONSE:'Thanks for the response.This is your ticket number: ',
+    CHAT:"Chat"
+    
+  };
