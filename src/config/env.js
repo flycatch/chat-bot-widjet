f1e51cce904578f1c5859d882150f4220a3e1415
Liju Kurikose <lijukrks166@gmail.com>
2022-11-22 19:39:35 +0530

    adding the new api https url

--- src/config/env.js
+++ src/config/env.js
@@ -1,15 +1,15 @@
 const env = {
   development: {
-    API_URL: 'http://142.93.222.51/api/v1',
-    BASE_URL: 'http://142.93.222.51/api/v1',
+    API_URL: 'https://support.api.flycatchtech.com/api/v1',
+    BASE_URL: 'https://support.api.flycatchtech.com/api/v1',
   },
   production: {
-    API_URL: 'http://142.93.222.51/api/v1',
-    BASE_URL: 'http://142.93.222.51/api/v1',
+    API_URL: 'https://support.api.flycatchtech.com/api/v1',
+    BASE_URL: 'https://support.api.flycatchtech.com/api/v1',
   },
   test: {
-    API_URL: 'http://142.93.222.51/api/v1',
-    BASE_URL: 'http://142.93.222.51/api/v1',
+    API_URL: 'https://support.api.flycatchtech.com/api/v1',
+    BASE_URL: 'https://support.api.flycatchtech.com/api/v1',
   },
 };
 
