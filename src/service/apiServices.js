76ab1742b8bde400d5b9c9f3cc3b8bdbdfb3cd50
cj-edwin <edwin.c@flycatchtech.com>
2024-07-31 14:33:36 +0530

    feat: sp-79 report issue feature in chat bot

--- src/service/apiServices.js
+++ src/service/apiServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const post = async function (url, body) {
   try {
     const resp = await axios.post(url, body);
-    return resp.data.data;
+    return resp.data;
   } catch (err) {}
 };
 
