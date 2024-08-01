daa4333364c8c256bcfc0b01305c67bc02ebccf7
cj-edwin <edwin.c@flycatchtech.com>
2024-08-01 15:33:10 +0530

    fix: special character issue fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,7 +18,7 @@ export const ChatBotConstants = {
       <br />
     </div>
   ),
-  TITLE_REQUEST: "Great, What is the subject or issue you’re experiencing?",
+  TITLE_REQUEST: "Great, What is the subject or issue you are experiencing?",
   USER_NAME_REQUEST: "Could you also please provide your full name?",
   EMAIL_REQUEST: "What is your email address?",
   DESCRIPTION_REQUEST: "Please describe the issue",
